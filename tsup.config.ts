4a36e94d860b280d420fd8b10df3d7ee408611c7
jhbhan <43838586+jhbhan@users.noreply.github.com>
2025-07-21 09:16:46 -0700

    tsup update to add css

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.tsx"],
+  format: ["esm", "cjs"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  external: ["react", "react-dom"],
+  injectStyle(css, fileId) {
+    if (fileId.endsWith("ParablesQuiz.css")) {
+      return css;
+    }
+    return "";
+  },
+});
